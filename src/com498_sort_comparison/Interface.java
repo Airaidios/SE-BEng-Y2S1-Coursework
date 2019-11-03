5dff5f2df9c993f0e6d41aee7605d2393191742a
Airaidios <airaidios@gmail.com>
2019-11-03 23:46:49 +0000

    Comparative analysis done
    Only thing left to do is input validation but I might just not do that

--- src/com498_sort_comparison/Interface.java
+++ src/com498_sort_comparison/Interface.java
@@ -1,6 +1,5 @@
 package com498_sort_comparison;
 
-import java.io.IOException;
 import java.util.*;
 
 /*
@@ -9,7 +8,8 @@ import java.util.*;
 
  */
 
-public class Interface {
+@SuppressWarnings({"DuplicatedCode", "InfiniteLoopStatement", "IntegerDivisionInFloatingPointContext"})
+class Interface {
 
     // For user input
     private final Scanner input = new Scanner(System.in);
@@ -17,9 +17,8 @@ public class Interface {
     // For generation of arrays to be sorted
     private static Integer[] arrayUnsorted;
     private static Integer[] arrayDuplicate;
-    private static Integer[] arraySorted;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         // Initialises interface instance, begins call chain
         Interface frontend = new Interface();
@@ -27,7 +26,7 @@ public class Interface {
 
     }
 
-    private void Start() throws IOException {
+    private void Start() {
 
         // Calls main menu, in while loop to facilitate continuous use
         while (true) {
@@ -39,7 +38,7 @@ public class Interface {
     }
 
     // Presents all available options to the user
-    private void mainMenu() throws IOException {
+    private void mainMenu() {
 
         String option;
 
@@ -87,14 +86,12 @@ public class Interface {
 
         }
 
-        return;
-
     }
 
     // Generate random, unsorted array of given size
     // If called intentionally by user (option 1 in mainMenu) allow for user input for these variables
     // Otherwise (called by sorting algorithms if there is no pre-existing unsorted array) use default values
-    private Integer[] generateArray(int mode) throws IOException {
+    private Integer[] generateArray(int mode) {
 
         int size = 0; // size of array
         String scenario; // case scenario (average or worst)
@@ -127,7 +124,7 @@ public class Interface {
         arrayUnsorted = new Integer[size];
 
         // Populate arrayUnsorted with random integers
-        for (int i = 0; i < size; i ++) { // for every index in arrayUnsorted
+        for (int i = 0; i < size; i++) { // for every index in arrayUnsorted
 
             arrayUnsorted[i] = random.nextInt(size + 1); // Inclusive upper bound
 
@@ -137,6 +134,13 @@ public class Interface {
 
             // I use built-in methods here as this is not part of the comparative analysis
             Arrays.sort(arrayUnsorted, Collections.reverseOrder());
+
+            if (arrayUnsorted.length % 2 != 0) { // if array has odd number of items, swap middle item with last item
+
+                double arrayHalf = arrayUnsorted.length / 2;
+                arrayUnsorted[(int) (Math.floor(arrayHalf) + 1)] = arrayUnsorted[arrayUnsorted.length - 1];
+
+            }
 
         }
 
@@ -149,11 +153,13 @@ public class Interface {
     // Simple bubble sort, uses arrayUnsorted
     // If mode == 1, trace sort, print at each step
     // Otherwise, just sort
-    private void bubbleSort(int mode) throws IOException {
+    private void bubbleSort(int mode) {
 
         // Check if arrayUnsorted is populated (or if user entered a stupid length value I guess)
         int temp;
-        boolean swapped = true;
+        int swaps = 0; // track how many swaps this algo performs
+        int passes = 0; // track how many passes this algo performs
+        int comparisons = 0; // track how many comparisons this algo performs
 
         if (arrayUnsorted ==  null) { // If array is not populated
 
@@ -161,17 +167,19 @@ public class Interface {
 
         }
 
-        for (int i = 0; i < arrayUnsorted.length; i ++) { // Duplicate array
+        duplicateArray(arrayUnsorted, arrayDuplicate);// Duplicate array
 
-            arrayDuplicate[i] = arrayUnsorted[i];
+        if (mode == 1) { // just to get initial state of array, for posterity
+
+            System.out.println(Arrays.toString(arrayDuplicate));
 
         }
 
-        System.out.println(Arrays.toString(arrayDuplicate)); // Print unsorted array for posterity I guess?
+        for (int i = 0; i < arrayDuplicate.length; i++) { // For every item in array
 
-        for (int i = 0; i < arrayDuplicate.length; i ++) { // For every item in array
+            for (int j = 1; j < (arrayDuplicate.length - i); j++) { // except items after the last swap of the pass
 
-            for (int j = 1; j < (arrayDuplicate.length - i); j ++) { // except items after the last swap of the pass
+                comparisons++;
 
                 if (arrayDuplicate[j - 1] > arrayDuplicate[j]) { // if previous item is greater than current item
 
@@ -179,23 +187,36 @@ public class Interface {
                     temp = arrayDuplicate[j - 1];
                     arrayDuplicate[j - 1] = arrayDuplicate[j];
                     arrayDuplicate[j] = temp;
-
-                    if (mode == 1) { // If algorithm needs to be traced
-
-                        System.out.println(Arrays.toString(arrayDuplicate)); // print current state of array
+                    swaps++;
 
                     }
 
                 }
 
+            if (mode == 1) { // If algorithm needs to be traced
+
+                System.out.println(Arrays.toString(arrayDuplicate)); // print current state of array
+
             }
 
+            passes++;
+
         }
+
+        System.out.println("Total passes: " + passes);
+        System.out.println("Total comparisons: " + comparisons);
+        System.out.println("Total swaps: " + swaps);
 
     }
 
     // If mode == 1, trace sort, print at each step, otherwise, just sort
-    private void selectSort(int mode) throws IOException {
+    @SuppressWarnings("DuplicatedCode")
+    private void selectSort(int mode) {
+
+        int minItem;
+        int swaps = 0; // track how many swaps this algo performs
+        int passes = 0; // track how many passes this algo performs
+        int comparisons = 0; // track how many comparisons this algo performs
 
         if (arrayUnsorted == null) {
 
@@ -203,21 +224,23 @@ public class Interface {
 
         }
 
-        for (int i = 0; i < arrayUnsorted.length; i ++) { // Duplicate array (could use .clone() but I don't want to)
+        duplicateArray(arrayUnsorted, arrayDuplicate);// Duplicate array
 
-            arrayDuplicate[i] = arrayUnsorted[i];
+        if (mode == 1) { // just to get initial state of array
+
+            System.out.println(Arrays.toString(arrayDuplicate));
 
         }
 
-        System.out.println(Arrays.toString(arrayDuplicate));
-
         // One by one move boundary of unsorted subarray
-        for (int i = 0; i < arrayDuplicate.length - 1; i ++) {
+        for (int i = 0; i < arrayDuplicate.length - 1; i++) {
 
             // Find the minimum element in unsorted array
-            int minItem = i;
+            minItem = i;
 
-            for (int j = i + 1; j < arrayDuplicate.length; j ++) {
+            for (int j = i + 1; j < arrayDuplicate.length; j++) {
+
+                comparisons++;
 
                 if (arrayDuplicate[j] < arrayDuplicate[minItem]) {
 
@@ -231,6 +254,7 @@ public class Interface {
             int temp = arrayDuplicate[minItem];
             arrayDuplicate[minItem] = arrayDuplicate[i];
             arrayDuplicate[i] = temp;
+            swaps++;
 
             if (mode == 1) { // Trace algo
 
@@ -238,13 +262,24 @@ public class Interface {
 
             }
 
+            passes ++;
+
         }
+
+        System.out.println("Total passes: " + passes);
+        System.out.println("Total comparisons: " + comparisons);
+        System.out.println("Total swaps: " + swaps);
 
     }
 
     // If mode == 1, trace sort, print at each step
     // Otherwise, just sort
-    private void shellSort(int mode) throws IOException {
+    private void shellSort(int mode){
+
+        int j;
+        int swaps = 0; // track how many swaps this algo performs
+        int passes = 0; // track how many passes this algo performs
+        int comparisons = 0;
 
         if (arrayUnsorted ==  null) { // If array is not populated
 
@@ -252,32 +287,38 @@ public class Interface {
 
         }
 
-        for (int i = 0; i < arrayUnsorted.length; i ++) { // Duplicate array
+        duplicateArray(arrayUnsorted, arrayDuplicate);// Duplicate array
 
-            arrayDuplicate[i] = arrayUnsorted[i];
+        if (mode == 1) { // just to get initial state of array
+
+            System.out.println(Arrays.toString(arrayDuplicate));
 
         }
 
-        System.out.println(Arrays.toString(arrayDuplicate));
-
         // Using original Shell gap sequence
-        for (int gap = arrayDuplicate.length / 2; gap > 0; gap /= 2)
-        {
+        for (int gap = arrayDuplicate.length / 2; gap > 0; gap /= 2) {
 
             // Do gapped insert sort for current gap size
-            for (int i = gap; i < arrayDuplicate.length; i += 1)
-            {
+            for (int i = gap; i < arrayDuplicate.length; i += 1) {
+
                 // add current item to the elements that have been gap sorted. Save item in temp and make a hole at
                 // current pos
                 int temp = arrayDuplicate[i];
 
+                comparisons ++;
+
                 // shift earlier gap-sorted elements up until the correct location for current item is found
-                int j;
-                for (j = i; j >= gap && arrayDuplicate[j - gap] > temp; j -= gap)
+                for (j = i; j >= gap && arrayDuplicate[j - gap] > temp; j -= gap) {
+
                     arrayDuplicate[j] = arrayDuplicate[j - gap];
+                    comparisons ++;
+                    swaps++;
+
+                }
 
                 // put temp in its correct location
                 arrayDuplicate[j] = temp;
+
             }
 
             if (mode == 1) { // Trace sort
@@ -286,15 +327,73 @@ public class Interface {
 
             }
 
+            passes++;
+
         }
+
+        System.out.println("Total passes: " + passes);
+        System.out.println("Total comparions: " + comparisons);
+        System.out.println("Total swaps: " + swaps);
 
     }
 
     // If mode == 1, trace sort, print at each step
     // Otherwise, just sort
-    private void compareAll() throws IOException {
+    private void compareAll() {
 
-        // TODO: comparative analysis of all algorithms
+        // for wall time
+        long startTime;
+        long endTime;
+        long elapsedTime;
+
+        if (arrayUnsorted ==  null) { // If array is not populated
+
+            generateArray(0); // populate it
+
+        }
+
+        duplicateArray(arrayUnsorted, arrayDuplicate); // Duplicate array
+
+        System.out.println("\n<---BUBBLE SORT--->"); // Measure bubble sort
+        startTime = System.nanoTime();
+
+        bubbleSort(0);
+
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+
+        System.out.println("Array sorted in " + elapsedTime + " nanoseconds (wall time)");
+
+        duplicateArray(arrayUnsorted, arrayDuplicate); // Duplicate array each time, as arrayDuplicate becomes sorted
+
+        System.out.println("\n<---SELECTION SORT--->"); // measure selection sort
+        startTime = System.nanoTime();
+
+        selectSort(0);
+
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+
+        System.out.println("Array sorted in " + elapsedTime + " nanoseconds (wall time)");
+
+        duplicateArray(arrayUnsorted, arrayDuplicate);
+
+        System.out.println("\n<---SHELL SORT (original gap sequence)--->"); // measure shell sort
+        startTime = System.nanoTime();
+
+        shellSort(0);
+
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+
+        System.out.println("Array sorted in " + elapsedTime + " nanoseconds (wall time)");
+
+    }
+
+    // Duplicate array
+    private void duplicateArray (Integer[] array, Integer[] arrayClone) {
+
+        System.arraycopy(array, 0, arrayClone, 0, array.length);
 
     }
 
